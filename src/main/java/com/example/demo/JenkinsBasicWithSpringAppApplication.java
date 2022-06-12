e25c70e5308b1f1127ad3f66e9e03d7c05055794
Ravi Gupta <0011223344ravi@gmail.com>
2022-06-12 15:26:59 +0530

    adding a restcontroller method

--- src/main/java/com/example/demo/JenkinsBasicWithSpringAppApplication.java
+++ src/main/java/com/example/demo/JenkinsBasicWithSpringAppApplication.java
@@ -6,8 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsBasicWithSpringAppApplication {
 	public  static final Logger logger = LoggerFactory.getLogger(JenkinsBasicWithSpringAppApplication.class);
 
@@ -15,6 +18,11 @@ public class JenkinsBasicWithSpringAppApplication {
 	public void intt() {
 		logger.info("Application started ...bhai");
 		
+	}
+	
+	@GetMapping("/getMessage")
+	public String getMessage() {
+		return "Deployment done successfully";
 	}
 
 	public static void main(String[] args) {
